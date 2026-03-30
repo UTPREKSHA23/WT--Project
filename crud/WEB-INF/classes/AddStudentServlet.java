b86cba4c36c39b1244b4a2d4ff93ba8fb9bdb426
Pragati Chaudhary <pragatichaudhary2107@gmail.com>
2026-03-30 19:38:40 +0530

    update addstudentservlet.java

--- crud/WEB-INF/classes/AddStudentServlet.java
+++ crud/WEB-INF/classes/AddStudentServlet.java
@@ -8,31 +8,69 @@ public class AddStudentServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
+        String action = request.getParameter("action");
+        String idStr = request.getParameter("id");
         String name = request.getParameter("name");
         String ageStr = request.getParameter("age");
 
         try {
-            int age = Integer.parseInt(ageStr);
-
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 
             Connection con = DriverManager.getConnection(
-                "jdbc:derby:testdb;create=true"
+                "jdbc:derby:/home/pragati/Downloads/apache-tomcat-9.0.116/bin/testdb;create=true"
             );
 
-            PreparedStatement ps = con.prepareStatement(
-                "insert into students (name, age) values (?, ?)"
-            );
+            Statement stmt = con.createStatement();
 
-            ps.setString(1, name);
-            ps.setInt(2, age);
+            // CREATE
+            if(action.equals("create")) {
+                int id = Integer.parseInt(idStr);
+                int age = Integer.parseInt(ageStr);
 
-            ps.executeUpdate();
+                stmt.executeUpdate("insert into students values (" + id + ", '" + name + "', " + age + ")");
+                out.println("Inserted Successfully!");
+            }
 
-            out.println("Data Inserted Successfully!");
+            // READ
+            else if(action.equals("read")) {
+                ResultSet rs = stmt.executeQuery("select * from students");
+
+                while(rs.next()) {
+                    out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3) + "<br>");
+                }
+            }
+
+            // UPDATE
+            else if(action.equals("update")) {
+                int id = Integer.parseInt(idStr);
+                int age = Integer.parseInt(ageStr);
+
+                stmt.executeUpdate("update students set name='" + name + "', age=" + age + " where id=" + id);
+                out.println("Updated Successfully!");
+            }
+
+            // DELETE
+            else if(action.equals("delete")) {
+                int id = Integer.parseInt(idStr);
+
+                stmt.executeUpdate("delete from students where id=" + id);
+                out.println("Deleted Successfully!");
+            }
+
+            // SEARCH
+            else if(action.equals("search")) {
+                int id = Integer.parseInt(idStr);
+
+                ResultSet rs = stmt.executeQuery("select * from students where id=" + id);
+
+                if(rs.next()) {
+                    out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3));
+                } else {
+                    out.println("Record Not Found");
+                }
+            }
 
             con.close();
 
